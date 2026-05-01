70e227d256fd48523a9a97bbd138b763961a5ef9
Jochen Deister <pattkopp@users.noreply.github.com>
2026-05-01 13:31:38 +0200

    feat: implement proper resut output in main.rs with newly defined error message

--- rust-json-parser/src/main.rs
+++ rust-json-parser/src/main.rs
@@ -1,9 +1,12 @@
 use rust_json_parser::parse_json;
 
 fn main() {
-    let input = r#""missing end quote"#;
+    let input = r#""The quick brown fox jumps over the lazy dog""#;
     let result = parse_json(input);
     println!("Input JSON: {input}");
     println!("\nResult:");
-    println!("{:?}", result);
+    match result {
+        Ok(value) => println!("Value: {:?}", value),
+        Err(err) => println!("Error: {}", err),
+    }
 }
