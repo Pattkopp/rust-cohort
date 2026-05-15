dc6bf0a19051376bf17b1a3ab0fd1c18988d8645
Jochen Deister <Pattkopp@users.noreply.github.com>
2026-05-15 13:18:50 +0200

    feat: add parse_json pyfunction with GIL-bound lifetime return type

--- rust-json-parser/src/python_bindings.rs
+++ rust-json-parser/src/python_bindings.rs
@@ -1,5 +1,5 @@
 // src/python_bindings.rs
-use crate::{JsonError, JsonValue};
+use crate::{JsonError, JsonParser, JsonValue};
 use pyo3::exceptions::{PyIOError, PyValueError};
 use pyo3::types::{PyDict, PyList};
 use pyo3::{IntoPyObjectExt, prelude::*};
@@ -31,20 +31,25 @@ impl From<JsonError> for PyErr {
 
 // Python-callable functions
 #[pyfunction]
-fn parse_json(py: Python<'_>, input: &str) -> PyResult<Bound<'_, PyAny>> { /* ... */
+// changed the signature because of https://github.com/PyO3/pyo3/discussions/4826
+fn parse_json<'py>(py: Python<'py>, input: &str) -> PyResult<Bound<'py, PyAny>> {
+    JsonParser::new().parse(input)?.into_bound_py_any(py)
 }
 
 #[pyfunction]
-fn parse_json_file(py: Python<'_>, path: &str) -> PyResult<Bound<'_, PyAny>> { /* ... */
+fn parse_json_file<'py>(py: Python<'py>, path: &str) -> PyResult<Bound<'py, PyAny>> {
+    todo!()
 }
 
 #[pyfunction]
 #[pyo3(signature = (obj, indent=None))]
-fn dumps(obj: &Bound<PyAny>, indent: Option<usize>) -> PyResult<String> { /* ... */
+fn dumps(obj: &Bound<PyAny>, indent: Option<usize>) -> PyResult<String> {
+    todo!()
 }
 
 // Helper (not exposed to Python)
-fn py_to_json_value(obj: &Bound<PyAny>) -> PyResult<JsonValue> { /* ... */
+fn py_to_json_value(obj: &Bound<PyAny>) -> PyResult<JsonValue> {
+    todo!()
 }
 
 // Module registration
